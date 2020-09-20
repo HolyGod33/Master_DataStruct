c9b9d229ab3ca694d0be18665e8ff38f9303a55c
cxy <1170712462@qq.com>
2020-09-20 21:06:58 +0800

    归并排序之合并

---
+++ MergeSort.cpp
@@ -0,0 +1,28 @@
+//
+// Created by cxy on 2020/9/20.
+//
+//归并排序
+
+#define N 10
+
+int *B = new int[N];
+void Merge(int A[], int low, int mid, int high){
+    int i, j ,k;
+    for (k = low; k < high; ++k) {
+        B[k] = A[k];
+    }
+
+    for (i = low, j = mid + 1; i <= mid && j <= high; ++k) {
+        if (B[i] <= B[j]){
+            A[k] = B[i++];
+        }
+        else{
+            A[k] = B[j++];
+        }
+    }while (i <= mid){
+        A[k++] = B[i++];
+    }
+    while (j <= high){
+        A[k++] = B[j++];
+    }
+}
